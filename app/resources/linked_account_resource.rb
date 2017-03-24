a514aaa6cbb3d35460b1162de659b27e2e86aac6
Peter Lejeck <peter.lejeck@gmail.com>
2017-03-23 22:59:12 -0700

    Add LinkedAccount::YoutubeChannel model hint

--- app/resources/linked_account_resource.rb
+++ app/resources/linked_account_resource.rb
@@ -1,5 +1,6 @@
 class LinkedAccountResource < BaseResource
   model_hint model: LinkedAccount::MyAnimeList
+  model_hint model: LinkedAccount::YoutubeChannel
 
   attributes :external_user_id, :token, :share_to,
     :share_from, :sync_to, :kind
